cac916182d465ba0f7a97241fbe94ddcc9e5e177
pionpill <673486387@qq.com>
2025-06-10 23:49:10 +0800

    :construction: 添加格式化配置

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import js from '@eslint/js'
-import globals from 'globals'
+import js from "@eslint/js";
+import globals from "globals";
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+
+export default defineConfig([
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ['**/*.{ts,tsx}'],
@@ -13,9 +15,15 @@ export default tseslint.config(
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'react': pluginReact,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
@@ -26,4 +34,4 @@ export default tseslint.config(
       'react-hooks/exhaustive-deps': 'off',
     },
   },
-)
+]);
