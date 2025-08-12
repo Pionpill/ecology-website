6e3225d9efc4e5a225fbb7919d14ee2884d2acd8
pionpill <673486387@qq.com>
2025-08-13 00:55:18 +0800

    :bug: 修复生物群落跳转 bug

--- src/app/wiki/biome/sidebar/index.tsx
+++ src/app/wiki/biome/sidebar/index.tsx
@@ -43,24 +43,24 @@ const BiomeSidebar: FC<BiomeSidebarProps> = (props) => {
   const navigate = useNavigate()
 
   const handleCategoryClick = (node: ListNode) => {
-    const isGroup = 'children' in node
-    const category = node.key.split(':')[0] as BiomeCategory
+    const isGroup = 'children' in GainNode
     if (isGroup) {
+      const category = node.key.split(':')[0] as BiomeCategory
       setBiomeFilter({ category: [category] }, true)
       navigate(`/wiki/biome/dashboard?category=${category}`)
     } else {
-      navigate(`/wiki/biome/${category}`)
+      navigate(`/wiki/biome/${node.key}`)
     }
   }
 
   const handleTagClick = (node: ListNode) => {
     const isGroup = 'children' in node
-    const tag = node.key.split(':')[0] as BiomeTag
     if (isGroup) {
+      const tag = node.key.split(':')[0] as BiomeTag
       setBiomeFilter({ tags: [tag] }, true)
       navigate(`/wiki/biome/dashboard?tag=${tag}`)
     } else {
-      navigate(`/wiki/biome/${tag}`)
+      navigate(`/wiki/biome/${node.key}`)
     }
   }
 
